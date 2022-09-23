e3c9068086ff5f1e743719b200314cc693d6a07a
Danie11l <113175907+Danie11l@users.noreply.github.com>
2022-09-23 17:13:24 +0700

    vinlie gntg
    tugas dari pak farisi gntg

---
+++ Ekskul coding 3,2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char y,n,buka,N;
+	
+	
+	cout<<"mau buka?\n";cin>>y;
+		
+	
+	cout<<"_______________________________________________________________________________________________\n"; 
+	cout<<"Dear                                                                                          |\n";
+	cout<<"Sahabat ku                                                                                    |\n";
+	cout<<"Sotoy....                                                                                     |\n\n";
+	
+	cout<<"Apakah kau baik baik saja Sotoy                                                               |\n\n";
+	cout<<"kamu pasti baik baik saja kan?                                                                |\n";
+	cout<<"Kalau iya bisa kah kita bertemu jam 7 dan kita ngumpul bersama teman-teman kita yang lamA     |\n";
+	cout<<"Apakah kau masih ingat?                                                                       |\n";
+	cout<<"jika iya, maukah kamu dateng ke tempat biasa kita nongkrong dulu?                             |\n";
+	cout<<"dan juga aku telah mengundang semua temen kita buat nongkrong                                 |\n";
+	cout<<"kalau bisa dateng Sotoy                                                                       |\n";
+	cout<<"kalau ngak dateng kudatangin rumah mu ;-;                                                     |\n\n";
+	
+	cout<<"salam                                                                                         |\n";
+	cout<<"Daniel                                                                                        |\n";
+	cout<<"______________________________________________________________________________________________|\n";
+	
+	
+
+	
+}
+
